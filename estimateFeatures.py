8e03a3a259308f924f14e4436e6fe98803f40115
jake <jake.labelle@hotmail.co.uk>
2019-01-16 10:02:21 +0000

    working -set some modes

--- estimateFeatures.py
+++ estimateFeatures.py
@@ -1,20 +1,13 @@
 import pickle
 import csv
-
+from math import fmod
        
 time = []
 sensor3 = []
+sensor2 = []
 with open("specifity", "rb") as f:
     specifity = pickle.load(f)
     
-with open("percision", "rb") as f:
-    percision = pickle.load(f)
-    
-with open("negativePercision", "rb") as f:
-    negativePercision = pickle.load(f)
-    
-with open("sensitivty", "rb") as f:
-    sensitivty = pickle.load(f)
 
 with open("s2features", "rb") as f:
     feature = pickle.load(f)
@@ -24,14 +17,9 @@ with open('rateData.csv', 'rt') as csvfile:
     for row in data:
         time.append(float(row[0]))
         sensor3.append(float(row[2]))
+        sensor2.append(float(row[3]))
         
 
-
-def getPercision(length, tempDiffSec):
-    b = int(tempDiffSec * -4000)
-    if(b < 0):
-        b = 0
-    return percision[length][b]
 
 def getSpecifity(length, tempDiffSec):
     b = int(tempDiffSec * -4000)
@@ -39,57 +27,161 @@ def getSpecifity(length, tempDiffSec):
         b = 0
     return specifity[length][b]
 
-def getSensitivty(length, tempDiffSec):
-    b = int(tempDiffSec * -4000)
-    if(b < 0):
-        b = 0
-    return sensitivty[length][b]
 
-def getThreshold(sensitivty,specifity):
-    a = 1
-    b = 1
-    return (sensitivty*a) + (specifity*b)
 
+def checkForFeature5(timelog):
+    g = len(sensor3log)-1
+    if(len(timelog) < 30):
+        return True
+    for timeback in range(30):
+        if(timelog[g - timeback] < timelog[g-timeback - 1]):
+            return True
+        elif(timelog[g - timeback] > timelog[g - timeback - 1] + 300):
+            return True
+    return False
+            
+    
+
+def checkForFeature1(sensor3log,timelog):
+    g = len(sensor3log)-1
+    highestSpecifity = 0
+    highestLength = 5
+    highestTempDiffSec = 0
+    for length in range(5,20):
+        timePassed = timelog[g] - timelog[g - length]
+        tempDiff = sensor3log[g] - sensor3log[g - length]
+        tempDiffSec = tempDiff / timePassed
+        cSpecifity = getSpecifity(length-5,tempDiffSec)
+        if(cSpecifity > highestSpecifity):
+            highestSpecifity = cSpecifity
+            highestLength = length
+            highestTempDiffSec = tempDiffSec
+    if(highestSpecifity > 0.99):
+        return [True,highestLength]
+    else:
+        return [False,0]
+
+def lengthOfFeature3(currentTemp):
+    return int(72.14327532 - (1.29644032*currentTemp))
+
+def feature3TempIncrease(timePassed):
+    return (timePassed * 0.00857402379537)
+
+def feature1TempDecrease(timePassed):
+    return (timePassed * -0.0259821576177)
+
+
+def lengthOfFeature0Heat(currentTemp):
+    return 773.8850914210703 - (currentTemp * 5.209158376824304)
+
+def lengthOfFeature0Cold(currentTemp):
+    return 667.9497021485934 + (currentTemp * 52.93806785791462)
+
+def heatingIncrease(currentTemp):
+    return 11 - (currentTemp * 0.16)
+    
+def coldingDecrease(currentTemp):
+    return 4.61 - (currentTemp  * 0.117)
+
+def heatingRatio(firstTime, lastTime, currentTime, heatingTime, coldingTime):
+    currentPhase = fmod(currentTime - firstTime,heatingTime + coldingTime)
+    lastPhase = fmod(lastTime - firstTime,heatingTime + coldingTime)
+    if(currentPhase > heatingTime and lastPhase < heatingTime):
+        timeHeating =  heatingTime - lastPhase;
+    elif(currentPhase > heatingTime and lastPhase > heatingTime):
+        timeHeating = 0.0;
+    elif(currentPhase < heatingTime and lastPhase < heatingTime):
+        timeHeating = currentPhase - lastPhase;
+    elif(currentPhase < heatingTime and lastPhase > heatingTime):
+        timeHeating = currentPhase;
+    ratio = (timeHeating)/(currentTime-lastTime)
+    return ratio
+
+def feature0TempChange(currentTemp, currentTime, firstTime, lastTime):
+    gainPerSecond = heatingIncrease(currentTemp) / lengthOfFeature0Heat(currentTemp)
+    print(currentTemp)
+    lossPerSecond = coldingDecrease(currentTemp) / lengthOfFeature0Cold(currentTemp)
+    heatRatio = heatingRatio(firstTime, lastTime, currentTime, lengthOfFeature0Heat(currentTemp), lengthOfFeature0Cold(currentTemp));	
+    gainThisTick = gainPerSecond*heatRatio * (currentTime-lastTime);
+    lossThisTick = lossPerSecond*(1-heatRatio) * (currentTime-lastTime);
+    tempChange = (gainThisTick + lossThisTick)
+    print(tempChange)
+    return tempChange
+     
 sensor3log = []
 sensor3feature = []
-sensor3time = []
+sensor2EstimateTemp = []
+timelog = []
+lastFeature1 = False
+feature3left = 0
+currentTemp = 50
+f0startTime = 0
 
 def recieveSensor(temp,t):
+
+    global lastFeature1
+    global feature3left
+    global currentTemp
+    global f0CycleStart
+    global f0startTime
+    
     sensor3log.append(temp)
-    sensor3time.append(t)
-    if(len(sensor3log) > 25):
-        g = len(sensor3log)-1
-        highestSpecifity = 0
-        highestLength = 5
-        highestTempDiffSec = 0
-        for length in range(5,20):
-            timePassed = sensor3time[g] - sensor3time[g - length]
-            tempDiff = sensor3log[g] - sensor3log[g - length]
-            tempDiffSec = tempDiff / timePassed
-            cSpecifity = getSpecifity(length-5,tempDiffSec)
-            if(cSpecifity > highestSpecifity):
-                highestSpecifity = cSpecifity
-                highestLength = length
-                highestTempDiffSec = tempDiffSec
-        if(highestSpecifity > 0.99):
-            sensor3feature.append(1)
-            for c in range(g - highestLength,g):
-                sensor3feature[c] = 1
-        else:
-            sensor3feature.append(0)
-    else:
+    timelog.append(t)
+    g = len(sensor3log)-1
+    
+    if(timelog[g] - f0startTime > 10000):
+        f0startTime = timelog[g]
+    if(feature3left >= 0):
+        sensor3feature.append(3)
+        currentTemp += feature3TempIncrease(timelog[g] - timelog[g-1])
+        feature3left -= 1;
+    elif(checkForFeature5(timelog)):
         sensor3feature.append(5)
-        
-        
-        
-for g in range(len(sensor3)):
+    else:
+        checkF1 = checkForFeature1(sensor3log,timelog)
+        if(checkF1[0]):
+            lastFeature1 = True
+            sensor3feature.append(1)
+            f0tof1 = 0
+            for length in range(checkF1[1]):
+                if(sensor3feature[g - length] != 1):
+                    sensor3feature[g-length] = 1
+                    f0tof1 += 1
+            currentTemp += feature1TempDecrease(timelog[g] - timelog[g - f0tof1 - 1])
+        elif(lastFeature1 == True):
+            lastFeature1 = False
+            feature3left = lengthOfFeature3(currentTemp)
+            currentTemp += feature0TempChange(currentTemp, timelog[g], f0startTime, timelog[g-1]) 
+            sensor3feature.append(0)
+        elif(lastFeature1 == False):
+            sensor3feature.append(0)
+            currentTemp += feature0TempChange(currentTemp, timelog[g], f0startTime, timelog[g-1]) 
+
+            
+    if(currentTemp > 59):
+        currentTemp = 59
+    elif(currentTemp < 35):
+        currentTemp = 35
+    sensor2EstimateTemp.append(currentTemp)
+    
+    
+    
+    
+    
+startnum = 23000
+num = 28500
+
+for g in range(startnum,num):
     recieveSensor(sensor3[g],time[g])
 
+#print(feature[startnum:num])
+#print(sensor3feature)
 
-    
-    
+import matplotlib.pyplot as plt
+plt.plot(timelog[0:num],sensor2EstimateTemp[0:num],"")
+plt.plot(timelog[0:num],sensor2[startnum:num],"")                    
+plt.show()
 
-    
     
 
 
--- sensor2features.py
+++ sensor2features.py
@@ -80,5 +80,58 @@ for g in range(len(time) - 60):
     elif((time[g] > float(1514590000)) & (time[g] < float(1514710000))):
         feature[g] = 5
     
+#print(feature)
+
 with open("s2features", "wb") as f:
     pickle.dump(feature, f, pickle.HIGHEST_PROTOCOL)
+    
+    
+tempvsLength = []
+TempLossPerSec3 = []
+TempLossPerSec1 = []
+look1start = True
+look1end = False
+look3start = False
+look3end = False
+start1 = 0
+start3 = 0
+end1 = 0
+end3 = 0
+for i in range(len(feature)):
+    if(look1start):
+        if(feature[i] == 1):
+            start1 = i
+            look1start = False
+            look1end = True
+    elif(look1end):
+        if(feature[i] != 1):
+            end1 = i
+            look1end = False
+            look3start = True
+    elif(look3start):
+        if(feature[i] == 3):
+            start3 = i
+            look3start = False
+            look3end = True
+    elif(look3end):
+        if(feature[i] != 3):
+            end3 = i
+            look3end = False
+            look1start = True
+            tempvsLength.append([sensor2[start3],end3 - start3])
+            TempLossPerSec3.append((sensor2[end3] - sensor2[start3]) / (time[end3] - time[start3]))
+            TempLossPerSec1.append((sensor2[end1] - sensor2[start1]) / (time[end1] - time[start1]))
+
+            
+import numpy as np
+
+print(np.mean(TempLossPerSec1))
+print(np.mean(TempLossPerSec3))
+
+print(np.polyfit(np.column_stack(tempvsLength)[0],np.column_stack(tempvsLength)[1],1))
+
+#0.00857402379537
+#[ -1.29644032  72.14327532]
+
+        
+        
